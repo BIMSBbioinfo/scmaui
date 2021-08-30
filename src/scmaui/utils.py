1923bb17bec7fd8d623b6e66b4a1648fb4bc4bf9
wkopp <wolfgang.kopp@mdc-berlin.de>
2021-08-30 17:43:47 +0200

    adapted  utils

--- src/scmaui/utils.py
+++ src/scmaui/utils.py
@@ -10,34 +10,57 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-def resnet_vae_batch_params(adata, batchnames):
-    columns = batchnames
-    ncats = [adata[list(adata.keys())[0]].obs[c].unique().shape[0] for c in columns]
-    params = OrderedDict([
-       ('batchnames', columns),
-       ('nbatchcats', ncats),
-         ])
-    return params
-
-
-def resnet_vae_params(args):
+def init_model_params():
     params = OrderedDict(
       [
-        ('nlayers_d', args.nlayers_d),
-        ('nhidden_e', args.nhidden_e),
-        ('nlayers_e', args.nlayers_e),
-        ('nhiddendecoder', args.nhidden_d),
-        ('inputdropout', args.inputdropout),
-        ('hidden_e_dropout', args.hidden_e_dropout),
-        ('hidden_d_dropout', args.hidden_d_dropout),
-        ('nhiddenbatcher', args.nhidden_b),
+        ('nhidden_e', 32),
+        ('nlayers_e', 5),
+        ('nhiddendecoder', 20),
+        ('nlayers_d', 1),
+        ('inputdropout', 0.1),
+        ('hidden_e_dropout', 0.0),
+        ('hidden_d_dropout', 0.0),
+        ('nhiddenbatcher', 128),
         ('nlayersbatcher', 2),
         ('nlasthiddenbatcher', 5),
-        ('latentdims', args.nlatent),
-        ('losses', args.loss),
-        ('nmixcomp', args.nmixcomp),
+        ('latentdims', 10),
+        ('nmixcomp', 1),
       ]
     )
+
+    return params
+   
+def get_model_params(dataset, args=None):
+    params = init_model_params()
+
+    modalities = dataset.modalities()
+    params['inputmodality'] = modalities[0]
+    params['outputmodality'] = modalities[1]
+
+    params.update(dataset.adversarial_config())
+    params.update(dataset.conditional_config())
+    params.update({'losses': dataset.losses})
+
+    if args is not None:
+
+        nparams = OrderedDict(
+          [
+            ('nlayers_d', args.nlayers_d),
+            ('nhidden_e', args.nhidden_e),
+            ('nlayers_e', args.nlayers_e),
+            ('nhiddendecoder', args.nhidden_d),
+            ('inputdropout', args.inputdropout),
+            ('hidden_e_dropout', args.hidden_e_dropout),
+            ('hidden_d_dropout', args.hidden_d_dropout),
+            ('nhiddenbatcher', args.nhidden_b),
+            ('nlayersbatcher', 2),
+            ('nlasthiddenbatcher', 5),
+            ('latentdims', args.nlatent),
+            ('losses', args.loss),
+            ('nmixcomp', args.nmixcomp),
+          ]
+        )
+        params.update(nparams)
 
     return params
 
@@ -143,7 +166,6 @@ class MultiLogisticRegression:
 
         for i in range(epochs):
             loss = self.update(X, y)
-            #print(f' e {i}: {loss}')
 
 
 def get_variable_regions(adata, groupby="louvain",
