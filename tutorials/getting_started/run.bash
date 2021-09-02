491e48fd681008549a3abf3718b942545255eb0f
wkopp <wolfgang.kopp@mdc-berlin.de>
2021-09-02 12:20:14 +0200

    added getting started tutorial

---
+++ tutorials/getting_started/run.bash
@@ -0,0 +1,3 @@
+jupyter nbconvert --to notebook --inplace --execute 01_getting_started_singleomics.ipynb
+jupyter nbconvert --to notebook --inplace --execute 02_getting_started_multiomics.ipynb
+jupyter nbconvert --to notebook --inplace --execute 03_running_scmaui_from_command_line.ipynb
