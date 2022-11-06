45093ca80ba3a3ca0e4407e09d97c0438d8e6ab9
hanyangii <y.jeong@dkfz-heidelberg.de>
2022-11-06 13:47:14 +0100

    separate encoder joint and KL in test run

--- src/scmaui/vae_models.py
+++ src/scmaui/vae_models.py
@@ -126,9 +126,6 @@ class VAE(tf.keras.Model):
             else:
                 z = ret
 
-            #for i, l in enumerate(self.encoder.losses):
-            #    losses[f'encl{i}']=l
-            # encoder returns both KL and joint mean loss
             losses['encoder_joint_mean'] = self.encoder.losses[0]
             losses['kl'] = self.encoder.losses[1] 
 
@@ -137,7 +134,7 @@ class VAE(tf.keras.Model):
             # Decoder returns a loss value for each modality 
             losses['recon'] = sum(self.decoder.losses)
 
-            losses['loss'] = losses['kl'] + losses['recon'] + losses['encoder_joint_mean']
+            losses['loss'] = losses['kl'] + losses['recon'] #+ losses['encoder_joint_mean']
 
             if 'adv' in losses:
                 losses['loss'] -= losses['adv']
@@ -171,7 +168,8 @@ class VAE(tf.keras.Model):
         else:
             z = ret
 
-        losses['kl'] = sum(self.encoder.losses)
+        losses['encoder_joint_mean'] = self.encoder.losses[0]
+        losses['kl'] = self.encoder.losses[1] 
 
         pred = self.decoder([z, odata, omask, intercept, conditional, adversarial], training=False)
 
